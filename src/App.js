092c5892b604b1335cd4d2edcdf3a4bd22de8940
Emmacodec <danielemmanuel7553@gmail.com>
2023-07-11 14:08:24 +0100

    update on my weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import axios from "axios";
 
 function App() {
+
+const url ='https://pro.openweathermap.org/data/2.5/forecast/climate?q=abuja,+234&appid=959e70ce52d49f591068ad7debc47a20'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      DAN
     </div>
   );
-}
+  }
+
 
 export default App;
